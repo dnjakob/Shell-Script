dbcbe80e68b4452d2121cec0ff18ae150c2ed0ae
Danjka99 <jakob_daniel@gmx.net>
2023-02-27 09:13:19 +0100

    sync

---
+++ Uebungen/Uebung04_2_lsg.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -e
+
+mins=$1
+
+if [[ -z $mins ]]; then
+    echo "Falsche Angabe! Benutzung: $0 [Minuten]"
+    exit 1
+fi
+
+hours=$(echo -n $(date +'%H %M') | cut -d" " -f1)
+minutes=$(echo -n $(date +'%H %M') | cut -d" " -f2)
+
+zeitmins=$(( "$hours" * 60 + "$minutes" ))
+
+echo "Test: $hours $minutes"
