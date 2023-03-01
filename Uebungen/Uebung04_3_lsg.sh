48ee11610232858484e9d57eb0dc39750cb04941
Danjka99 <jakob_daniel@gmx.net>
2023-03-01 13:20:36 +0100

    sync

--- Uebungen/Uebung04_3_lsg.sh
+++ Uebungen/Uebung04_3_lsg.sh
@@ -5,22 +5,19 @@ b=
 c=
 
 read -p "Gib die Länge von a ein: " a
-
-if [[ -z $a ]]; then
-    echo "Ungültige Eingabe! Geben Sie einen Wert für a ein!"
-    exit 1
-fi
-
 read -p "Gib die Länge von b ein: " b
-
-if [[ -z $b ]]; then
-    echo "Ungültige Eingabe! Geben Sie einen Wert für b ein!"
-    exit 1
-fi
-
 read -p "Gib die Länge von c ein: " c
 
-if [[ -z $c ]]; then
-    echo "Ungültige Eingabe! Geben Sie einen Wert für c ein!"
+if [[ -z $a || -z $b || -z $c ]]; then
+    echo "Ungültige Eingabe! Geben Sie einen Wert für a, b und c ein!"
     exit 1
 fi
+
+
+if [[ $(( $a + $b >= $c )) && $(( $b + $c >= $a )) && $(( $a + $c >= $b )) ]]; then
+    echo "Dieses Dreieck ist konstruierbar!"
+else
+    echo "Dieses Dreieck ist nicht konstruierbar! Bitte logische Werte eintragen!"
+fi
+
+exit 0
