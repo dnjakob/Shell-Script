acad5d6da733e44e3837784c9ef66524f207ac76
Danjka99 <jakob_daniel@gmx.net>
2023-03-17 11:46:18 +0100

    sync

---
+++ Uebungen/Kurzarbeit II.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -e
+
+i=
+j=
+k=
+
+for ((i = 0; i <= 15; i++)); do
+    for ((j = 0; j <= 15; j++)); do
+        k=$(( $i * $j ))
+        echo -n "$(tput setaf $k)VG $(tput sgr0)"
+    done
+    echo \n
+done
+
+for ((i = 0; i <= 15; i++)); do
+    for ((j = 0; j <= 15; j++)); do
+        k=$(( $i * $j ))
+        echo -n "$(tput setab $k)HG $(tput sgr0)"
+    done
+    echo \n
+done
+
+exit 0
