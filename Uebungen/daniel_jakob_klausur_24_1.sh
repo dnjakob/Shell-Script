eef41a6f1453d386f4d2813c65893291c2650d06
Danjka99 <jakob_daniel@gmx.net>
2024-01-30 11:41:45 +0100

    sync

--- Uebungen/daniel_jakob_klausur_24_1.sh
+++ Uebungen/daniel_jakob_klausur_24_1.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
 
-memtotal=$(cat /proc/meminfo | grep 'MemTotal: ' | cut -f2 -d: )
-memfree=$(cat /proc/meminfo | grep 'MemFree: ' | cut -f2 -d: )
-memavailable=$(cat /proc/meminfo | grep 'MemAvailable: ' | cut -f2 -d: )
-active=$(cat /proc/meminfo | grep 'Active: ' | cut -f2 -d: )
-inactive=$(cat /proc/meminfo | grep 'Inactive: ' | cut -f2 -d:)
-swaptotal=$(cat /proc/meminfo | grep 'SwapTotal: ' | cut -f2 -d: )
-swapfree=$(cat /proc/meminfo | grep 'SwapFree: ' | cut -f2 -d: )
+memtotal=$(cat /proc/meminfo | grep 'MemTotal: ' | cut -f2 -d: | awk '{ print$1 }' )
+memfree=$(cat /proc/meminfo | grep 'MemFree: ' | cut -f2 -d: | awk '{ print$1 }' )
+memavailable=$(cat /proc/meminfo | grep 'MemAvailable: ' | cut -f2 -d: | awk '{ print$1 }' )
+active=$(cat /proc/meminfo | grep 'Active: ' | cut -f2 -d: | awk '{ print$1 }' )
+inactive=$(cat /proc/meminfo | grep 'Inactive: ' | cut -f2 -d: | awk '{ print$1 }' ) 
+swaptotal=$(cat /proc/meminfo | grep 'SwapTotal: ' | cut -f2 -d: | awk '{ print$1 }' )
+swapfree=$(cat /proc/meminfo | grep 'SwapFree: ' | cut -f2 -d: | awk '{ print$1 }' )
 
-echo "Gesamter-Arbeitsspeicher:"$memtotal
-echo "Freier Arbeitsspeicher:"$memfree
-echo "Verfügbarer Arbeitsspeicher ohne Swap:"$memavailable
-echo "Belegter Speicher:"$active
-echo "Speicher, der wieder belegt werden kann:"$inactive
-echo "Swap-Speicher:"$swaptotal
-echo "Freier Swap-Speicher:"$swapfree
+memtotal=$(( $memtotal / 1024 ))
+memfree=$(( $memfree / 1024 ))
+memavailable=$(( $memavailable / 1024 ))
+active=$(( $active / 1024 ))
+inactive=$(( $inactive / 1024 ))
+swaptotal=$(( $swaptotal / 1024 ))
+swapfree=$(( $swapfree / 1024 ))
+
+echo "Gesamter-Arbeitsspeicher: "$memtotal" MB"
+echo "Freier Arbeitsspeicher: "$memfree" MB"
+echo "Verfügbarer Arbeitsspeicher ohne Swap: "$memavailable" MB"
+echo "Belegter Speicher: "$active" MB"
+echo "Speicher, der wieder belegt werden kann: "$inactive" MB"
+echo "Swap-Speicher: "$swaptotal" MB"
+echo "Freier Swap-Speicher: "$swapfree" MB"
