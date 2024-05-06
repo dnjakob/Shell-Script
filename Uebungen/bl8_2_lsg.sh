3f29bebc5cb69cd703b85404b3bfd36f252612d1
dnjakob <126343846+dnjakob@users.noreply.github.com>
2024-05-06 12:44:06 +0200

    sync

--- Uebungen/bl8_2_lsg.sh
+++ Uebungen/bl8_2_lsg.sh
@@ -2,7 +2,10 @@
 
 source_dir="/home/matthias/Dokumente"
 backup_dir="/home/matthias/Backup"
+# spezieller Timestamp)
 timestamp=$(date +%d%m%Y%H%M)
+# zusammengesetzter Dateiname
 backup_file="backup_$timestamp.tar.gz"
 
+# eigentlicher Tar-Befehl: -(c)reate, z(compress), (v)erbose, (f)ilename
 tar -czvf "$backup_dir/$backup_file" "$source_dir"
